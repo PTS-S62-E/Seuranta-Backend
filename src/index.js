dc77605f4bf1bc39a20fb0535555794f21c28cca
Sander Geraedts <sander@silverbee.nl>
2018-06-18 10:48:38 +0200

    monitoring done + frontend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import 'babel-polyfill';
 import request from 'request';
+import path from 'path';
 
 const fs = require('fs');
 
@@ -15,37 +16,54 @@ app.use(function (request, response, next) {
 
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
+const pollingTime = process.env.POLLINGTIME || 15 * 60 * 1000;
 
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starts server
 server.listen(port, function () {
     console.log('Server starter on port ' + port + '...');
 
-    setInterval(function () {
-        request.get('http://192.168.24.100:8082/Politie-Backend/api/status/',
-            function (error, response, body) {
-                const currentTime = new Date();
-                const status = (response.statusCode === 200) ? 'RUNNING' : 'DOWN';
+    pollServices();
 
-                fs.appendFile('status.log', `${currentTime}|POLITIE-BACKEND|${status}\n`, function (err) {
-                    if (err) throw err;
-                });
-            });
-
-
-        request.get('http://192.168.24.100:8082/rekening-administratie/api/status/',
-            function (error, response, body) {
-                const currentTime = new Date();
-                const status = (response.statusCode === 200) ? 'RUNNING' : 'DOWN';
-
-                fs.appendFile('status.log', `${currentTime}|REKENING-ADMINISTRATIE|${status}\n`, function (err) {
-                    if (err) throw err;
-                });
-            });
-
-    }, 15 * 60 * 1000);
+    setInterval(pollServices, pollingTime);
 });
+
+function pollServices() {
+    request.get('http://192.168.24.100:8082/Politie-Backend/api/status/',
+        function (error, response, body) {
+            const currentTime = new Date();
+            const status = (response.statusCode === 200) ? 'RUNNING' : 'DOWN';
+
+            fs.appendFile('lib/public/status.log', `${currentTime}|POLITIE-BACKEND|${status}\n`, function (err) {
+                if (err) throw err;
+            });
+        });
+
+
+    request.get('http://192.168.24.100:8082/rekening-administratie/api/status/',
+        function (error, response, body) {
+            const currentTime = new Date();
+            const status = (response.statusCode === 200) ? 'RUNNING' : 'DOWN';
+
+            fs.appendFile('lib/public/status.log', `${currentTime}|REKENING-ADMINISTRATIE|${status}\n`, function (err) {
+                if (err) throw err;
+            });
+        });
+
+
+    request.get('http://192.168.24.100:8082/registratie-verplaatsing/api/status/',
+        function (error, response, body) {
+            const currentTime = new Date();
+            const status = (response.statusCode === 200) ? 'RUNNING' : 'DOWN';
+
+            fs.appendFile('lib/public/status.log', `${currentTime}|REGISTRATIE-VERPLAATSING|${status}\n`, function (err) {
+                if (err) throw err;
+            });
+        });
+}
